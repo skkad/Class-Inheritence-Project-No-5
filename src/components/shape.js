35e98a62b5cc8e1d2bcac0dea63a5026d54bd341
skkad <skkad1998@gmail.com>
2022-06-07 21:33:39 +0530

    First commit

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,21 @@
 // declare class
+class Shape{
+
+    constructor(color){
+        if(color===undefined || color===null){
+            this.color = 'purple'
+        }else{
+            this.color = color
+        }
+    }
+    drawShape() {
+        
+    }
+    calculateArea(){
+
+    }
+
+}
 
 // export class using module.exports
+module.exports = Shape
