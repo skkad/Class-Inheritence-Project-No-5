741511230133da775c7e9eb7f64a9f6bc6f91766
skkad <skkad1998@gmail.com>
2022-06-10 22:01:19 +0530

    second commit

---
+++ circle.js
@@ -0,0 +1,16 @@
+// import using require
+const shape = require('./shape.js')
+// declare class
+class Circle extends Shape{
+    constructor(color){
+        super(color)
+    }
+
+    calculateArea(){
+        return 2*(Math.PI)*(this.radius*this.radius)
+    }
+}
+
+
+// export class using module.exports
+module.exports = Circle
